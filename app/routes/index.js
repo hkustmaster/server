ddde842ad222f4f084879cb14d8a42759e039c8f
mavericklam <HuenLam@Mavericks-MBP.lan>
2015-12-08 09:47:43 +0800

    bug

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,13 +9,36 @@ var Upload = require('../controllers/upload');
 /* GET home page. */
 var multer  = require('multer')
 var path = require('path');
+var jwt = require('jwt-simple');
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, '../upload'))
   },
   filename: function (req, file, cb) {
+  	var token = req.body.token
+  	//decode the token
+  	if(!token)
+    	return res.json({message:'Not Sign In'})
+  	var decoded = jwt.decode(token, tokenKey);
+  	if (decoded.exp <= Date.now()) {
+    	res.json({message:'Access token has expired,sign in again'});
+  	}
+  	else
+	    User.findOne({ _id: decoded._id }, function(err, user) {
+	      if (err){
+	        return res.json({message:'Sever Error'})
+	      }
+	      else if(!user){
+	        return res.json({message:'Invaild Token'})
+	      }
+	      else{
+	        req.user = user;
+		    delete req.body.token
+	        next()
+	      }
+	    });
   	console.log("test body"+req.body.token+"file"+file)
-    cb(null, req.body.token+'.'+req.body.ext)
+    cb(null, req.user._id+'.'+req.body.ext)
   }
 })
 
