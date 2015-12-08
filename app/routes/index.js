de1c85074923eccee33b8d57102b5c773a9d3606
mavericklam <HuenLam@Mavericks-MBP.lan>
2015-12-08 09:27:50 +0800

    bug

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,7 +14,7 @@ var storage = multer.diskStorage({
     cb(null, path.join(__dirname, '../upload'))
   },
   filename: function (req, file, cb) {
-  	console.log("test body"req.body)
+  	console.log("test body"+req.body)
     cb(null, req.body.token+'.'+req.body.ext)
   }
 })
