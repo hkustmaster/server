99f63f352ddc88f6e9e97c53a8a30b31f02f64b0
mavericklam <HuenLam@Mavericks-MBP.lan>
2015-12-08 23:36:46 +0800

    vote

--- app/controllers/intereact.js
+++ app/controllers/intereact.js
@@ -49,7 +49,7 @@ exports.vote=function(req,res){
 	var actid=req.body.actid
 	var thevote=req.body.vote
 	console.log(thevote)
-	activity.findOneAndUpdate({hid:actid,"participants.id":req.user._id},{$set:{participants.$.availdableAt:thevote}},function(err,act){
+	activity.findOneAndUpdate({hid:actid,"participants.id":req.user._id},{$set:{"participants.$.availdableAt":"fuck mongoose"}},function(err,act){
 		if(err)
 			res.json({message:"Server Error"})
 		else
