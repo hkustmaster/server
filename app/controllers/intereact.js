edaaba60f3556b79c1a65df5aa6f57831ff8b373
mavericklam <HuenLam@Mavericks-MBP.lan>
2015-12-08 23:21:56 +0800

    vote

--- app/controllers/intereact.js
+++ app/controllers/intereact.js
@@ -48,7 +48,6 @@ console.log(err)
 exports.vote=function(req,res){
 	var actid=req.body.actid
 	var thevote=req.body.vote
-	console.log(vote)
 	activity.findOneAndUpdate({hid:actid,"participants.id":req.user._id},{$set:{"participants.$.availdableAt":thevote}},function(err,act){
 		if(err)
 			res.json({message:"Server Error"})
