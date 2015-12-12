5a61938964bc3e2ba392c41671b363952259ee00
mavericklam <HuenLam@Mavericks-MacBook-Pro.local>
2015-12-12 12:39:59 +0800

    comment

--- app/controllers/comment.js
+++ app/controllers/comment.js
@@ -46,7 +46,7 @@ exports.post = function(req, res) {
 //get comments of an activity
 exports.getComments=function(req,res){
   var activityId=req.body.id
-  Comment.find({activity:activityId}).sort("createAt").populate("from","name").exec(function(err,comments){
+  Comment.find({activity:activityId}).sort("createAt").populate("from.id","name").exec(function(err,comments){
     if(err)
       console.log(err)
     else
